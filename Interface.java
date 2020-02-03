e3a9b2d41b734b623adfbd10e378bccba4859942
Aarya Patel <aarya.patel@gmail.com>
2020-02-03 09:26:55 -0500

    Added Interface.java

---
+++ Interface.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Interface {
+	private JFrame frame;
+
+	public Interface() {
+		frame = new JFrame();
+		frame.setSize(300, 300);
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new Interface();[
+	}
+}
