ab06d7703b8a99529d4739b805e82b35c8314351
Aarya Patel <aarya.patel@gmail.com>
2020-02-03 12:05:39 -0500

    Renamed Interface.java to Tester.java

---
+++ Tester.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Tester {
+	private JFrame frame;
+
+	public Tester() {
+		frame = new JFrame();
+		frame.setSize(300, 300);
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new Interface();[
+	}
+}
